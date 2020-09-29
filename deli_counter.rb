70c042f2b4a1d40c9c6818d8d76cfdd5a987f878
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-29 18:13:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,31 @@
 katz_deli = []
-def line(deli_line)
-  katz_deli = []
-  if deli_line.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    deli_line.each.with_index(1) do |name, index|
-      katz_deli.push("#{index}. #{name}")
+    deli_line = []
+    katz_deli.each.with_index(1) do |name, index|
+      deli_line.push("#{index}. #{name}")
     end
-    puts "The line is currently: #{katz_deli.join(" ")}"
+    puts "The line is currently: #{deli_line.join(" ")}"
   end
 end
-def take_a_number(katz_deli, customer)
-  katz_deli.push(customer)
-  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+
+#def take_a_number(katz_deli, customer)
+   katz_deli.push(customer)
+     puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+ end
+ 
+$counter = 0
+def take_a_number(katz_deli)
+  $counter += 1
+  katz_deli.push($counter)
+    puts "Welcome, you are #{$counter}"
+  end
 end
-def now_serving (katz_deli)
+  
+
+def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
   else puts "Currently serving #{katz_deli[0]}."
